928a53be36db30a57c2ece7b7606f35167211ce9
Yosui Tagami <tagamy@gmail.com>
2012-12-30 02:23:10 +0900

    fix

--- conf/config.php
+++ conf/config.php
@@ -3,7 +3,7 @@
 define('DSN_MASTER', 'mysql:host=localhost;dbname=yopyphp');
 define('DSN_SLAVE1', 'mysql:host=localhost;dbname=yopyphp');
 
-define('DB_USER', 'yosy');
+define('DB_USER', 'yopyphp');
 define('DB_PASS', 'password');
 
 define('MCD1_HOST', 'localhost');
@@ -12,8 +12,8 @@ define('MCD1_PORT', '11211');
 define('KVS1_HOST', 'localhost');
 define('KVS1_PORT', '1978');
 
-define('TWITTER_CONSUMER_KEY',    'LuvkxM1PEuHTxyitXZ3mwQ');
-define('TWITTER_CONSUMER_SECRET', 'y3pVTJCSM8eE45Ktnx5BOfPJCbqpf9TXmPla7YN7DXA');
+define('TWITTER_CONSUMER_KEY',    '');
+define('TWITTER_CONSUMER_SECRET', '');
 
 define('DEBUG', 1);
 
